88e8a03107f9c75dde535238c060a9b4a439a14b
Sebastian Kunz <sebi.kunz92@gmail.com>
2023-07-08 14:25:25 +0200

    added error handling, password field is type password now, fielads are form now

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -3,9 +3,11 @@
         <v-row align="center" justify="center">
             <v-col>
                 <v-img height="150" :src="require('@/assets/logo.png')" width="100%" aspect-ratio="1"></v-img>
-                <v-text-field label="username" prepend-inner-icon="mdi-account" filled rounded dense v-model="username"></v-text-field>
-                <v-text-field label="password" prepend-inner-icon="mdi-lock" filled rounded dense v-model="password"></v-text-field>
-                <v-btn class="mt-5" rounded outlined color="blue" width="100%" @click="login(username, password)">Login</v-btn>
+                <v-form @submit="login">
+                  <v-text-field label="username" prepend-inner-icon="mdi-account" filled rounded dense v-model="username" :error-messages="usernameError"></v-text-field>
+                  <v-text-field label="password" prepend-inner-icon="mdi-lock" filled rounded dense v-model="password" type="password" :error-messages="passwordError"></v-text-field>
+                  <v-btn class="mt-5" rounded outlined color="blue" width="100%" type="submit">Login</v-btn>
+                </v-form>
             </v-col>
         </v-row>
     </v-container>
@@ -18,15 +20,28 @@ export default {
   data: () => ({
     username: '',
     password: '',
+    usernameError: '',
+    passwordError: '',
   }),
   methods: {
-    async login(username, password){
+    async login(){
+      if (this.validate()){
         const formData = new FormData()
-        formData.append('username', username)
-        formData.append('password', password)
+        formData.append('username', this.username)
+        formData.append('password', this.password)
         requests.post('/api/login', formData, false).then( () => {
            this.$router.push('/home')
-        }).catch((err) => {alert(err)})
+        }).catch((err) => {
+            this.usernameError = ' '
+            this.passwordError = err == 'Unauthorized' ? 'username or password incorrect' : err
+          }
+        )
+      }
+    },
+    validate(){
+      this.usernameError = this.username.trim().length == 0 ? 'enter username'  : ''
+      this.passwordError = this.password.trim().length == 0 ? 'enter password'  : ''
+      return (this.usernameError + this.passwordError).length == 0
     }
   }
 };
