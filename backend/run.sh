c70deea97dd391c9d62fea19aeed30a45d421e9a
Peter Schlorke <132813854+firefincher@users.noreply.github.com>
2023-08-23 20:43:32 +0200

    Update run.sh
    move docker run command to dev and prod mdoe
    
    add stop and remove dev db when pressing ctrl + c
    
    add persistent volume to prod mode

--- backend/run.sh
+++ backend/run.sh
@@ -23,21 +23,31 @@ then
     fi
     echo "[] Setting up python environment"
     python3 -m venv env && source env/bin/activate && cd server && pip3 install -q -r requirements.txt
-    echo "[] Setting up database (mongodb) via Docker"
-    if [ ! "$(docker ps -a | grep linqua-db)" ]
-    then
-         docker run -d -e MONGO_INITDB_ROOT_USERNAME='linqua_db_admin' -e MONGO_INITDB_ROOT_PASSWORD='dev_password' -p 27017:27017 --name linqua-db mongo:latest  
-    fi
     echo "LINQUA SETUP COMPLETE"
 elif test "$1" = "dev"
 then
     echo "DEVELOPMENT MODE!!!! DO NOT USE IN PRODUCTION!!!"
-    docker start linqua-db
     echo "WARNING: Data of linqua dev database is only saved inside container (not persistent)"
-    source "${SCRIPTPATH}/env/bin/activate" && cd $SERVERPATH && python3 -m uvicorn server:app --reload
+    echo "[] Setting up non persistent database (mongodb) via Docker"
+    db () (
+        trap - INT
+        if [ ! "$(docker ps -a | grep linqua-db)" ]
+        then
+            docker run -e MONGO_INITDB_ROOT_USERNAME='linqua_db_admin' -e MONGO_INITDB_ROOT_PASSWORD='dev_password' -p 27017:27017 --name linqua-db mongo:latest &
+        fi
+        source "${SCRIPTPATH}/env/bin/activate" && cd $SERVERPATH && python3 -m uvicorn server:app --reload
+    )
+    db
+    docker stop linqua-db
+    docker rm linqua-db
 elif test "$1" = "prod"
 then
     echo "LINQUA PRODUCTION MODE"
+    echo "[] Setting up persistent database (mongodb) via Docker"
+    if [ ! "$(docker ps -a | grep linqua-db)" ]
+    then
+         docker run -d -e MONGO_INITDB_ROOT_USERNAME='linqua_db_admin' -e MONGO_INITDB_ROOT_PASSWORD='dev_password' -v $SCRIPTPATH/server/database:/data/db -p 27017:27017 --name linqua-db mongo:latest
+    fi    
     source "${SCRIPTPATH}/env/bin/activate" && cd $SERVERPATH && python3 -m uvicorn server:app
 else
     echo -e "OTPIONS:\n\t- setup:  \tsetup python project\n\t- dev:  \tstart server in dev mode (reoload in case of file changes)\n\t- prod:  \tstart server in production mode"
